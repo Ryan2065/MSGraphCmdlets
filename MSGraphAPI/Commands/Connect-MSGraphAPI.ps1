58e144c13cbf994a7e6d8fa652aae2a75593623f
Ryan Ephgrave <RYan2065@gmail.com>
2018-03-09 15:52:49 -0600

    Added param attributes

--- MSGraphAPI/Commands/Connect-MSGraphAPI.ps1
+++ MSGraphAPI/Commands/Connect-MSGraphAPI.ps1
@@ -1,12 +1,19 @@
 Function Connect-MSGraphAPI {
+    [CmdletBinding()]
     Param(
+        [Parameter(Mandatory=$true)]
         [string]$Tenant,
+        [Parameter(Mandatory=$true)]
         [string]$ClientId,
+        [Parameter(Mandatory=$true)]
         [string]$RedirectURI,
+        [Parameter(Mandatory=$true)]
         [string]$Resource,
+        [Parameter(Mandatory=$true)]
         [pscredential]$Credential,
         [ValidateSet('v1.0', 'beta')]
         [string]$GraphEndpoint = 'v1.0',
+        [Parameter(Mandatory=$false)]
         [switch]$ReturnClientObject
     )
     $Script:MSGraphClient = [MSGraphAPI.ConnectADAL]::GetGraphClientByUserAndPassword(
